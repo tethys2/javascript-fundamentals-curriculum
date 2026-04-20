d9e6f574dccba8b83dfd2c7abd54f1de1cbc28dc
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-04-20 00:37:20 +0000

    list_length

---
+++ lesson5/classwork/list_length.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "banana", "cherry", "date", "orange", "mango", "tomato", "jack fruit"];
+
+let length = fruits.length;
+console.log("Number of fruits: ", length);
+
+let last_fruit = fruits[length - 1];
+console.log("Last fruit:", last_fruit)
+
+
