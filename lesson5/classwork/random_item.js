e6ecf54783d18971c9faf3f917ba3ee8ed6fe882
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-04-20 00:44:16 +0000

    random_item

---
+++ lesson5/classwork/random_item.js
@@ -0,0 +1,8 @@
+let animals = ["cat", "dog", "rabbit", "hamster", "parrot", "red panda", "monkey", "human"];
+
+let length = animals.length;
+
+let random_index = Math.floor(Math.random() * length);
+let random_animal = animals[random_index];
+console.log("Random animal:", random_animal);
+
